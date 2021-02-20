5db17b635d8f6b9a24c57886b1ae733135628418
soumilagarwal <39463423+soumilagarwal@users.noreply.github.com>
2021-02-20 19:07:48 +0530

    Add files via upload

---
+++ Compiler Project/main.c
@@ -0,0 +1,22 @@
+/**
+*   @author: Soumil Agrawal
+*            Giridhar Bajpai
+*            Raunak Mantri
+*            Anuj Kharbanda
+*/
+
+#include "lexer.h"
+int main(int argc, char *argv[])
+{
+	if(argc<1)
+	{
+		printf("Proper Arguments not given\n");
+		return 0;
+	}
+
+	initialise_Hash();
+	create_Lexer();
+	printtokens(argv[1]);
+
+	return 0;
+}
